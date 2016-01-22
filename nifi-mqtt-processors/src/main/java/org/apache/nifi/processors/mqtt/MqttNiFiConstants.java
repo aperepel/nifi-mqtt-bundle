36665ef5622af2d2aa688819d2d18481ff238423
Andrew Grande <aperepel@gmail.com>
2016-01-22 11:37:33 -0500

    Exposed the MQTT Version selector

--- nifi-mqtt-processors/src/main/java/org/apache/nifi/processors/mqtt/MqttNiFiConstants.java
+++ nifi-mqtt-processors/src/main/java/org/apache/nifi/processors/mqtt/MqttNiFiConstants.java
@@ -1,8 +1,16 @@
 package org.apache.nifi.processors.mqtt;
 
 import org.apache.nifi.components.AllowableValue;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 
 public class MqttNiFiConstants {
+
+    /*
+      ------------------------------------------
+        Clean Session Values
+      ------------------------------------------
+     */
+
     public static final AllowableValue ALLOWABLE_VALUE_CLEAN_SESSION_TRUE =
             new AllowableValue("true", "Clean Session", "Client and Server discard any previous session and start a new " +
                                                                 "one. This session lasts as long as the network connection. " +
@@ -15,6 +23,14 @@ public class MqttNiFiConstants {
                                                           "CleanSession set to 0, the server stores further QoS 1 and QoS 2 messages that match any " +
                                                           "subscriptions that the client had at the time of disconnection as part of the session state");
 
+
+    /*
+      ------------------------------------------
+        QoS Values
+      ------------------------------------------
+     */
+
+
     public static final AllowableValue ALLOWABLE_VALUE_QOS_0 =
             new AllowableValue("0", "0 - At most once", "Best effort delivery. A message won’t be acknowledged by the receiver or stored and redelivered by the sender. " +
                                                         "This is often called “fire and forget” and provides the same guarantee as the underlying TCP protocol.");
@@ -26,4 +42,24 @@ public class MqttNiFiConstants {
     public static final AllowableValue ALLOWABLE_VALUE_QOS_2 =
             new AllowableValue("2", "2 - Exactly once", "Guarantees that each message is received only once by the counterpart. It is the safest and also " +
                                                         "the slowest quality of service level. The guarantee is provided by two round-trip flows between sender and receiver.");
+
+
+    /*
+      ------------------------------------------
+        MQTT Version Values
+      ------------------------------------------
+     */
+    public static final AllowableValue ALLOWABLE_VALUE_MQTT_VERSION_AUTO =
+            new AllowableValue(String.valueOf(MqttConnectOptions.MQTT_VERSION_DEFAULT),
+                                      "AUTO",
+                                      "Start with v3.1.1 and fallback to v3.1.0 if not supported by a broker");
+
+    public static final AllowableValue ALLOWABLE_VALUE_MQTT_VERSION_311 =
+            new AllowableValue(String.valueOf(MqttConnectOptions.MQTT_VERSION_3_1_1),
+                                      "v3.1.1");
+
+    public static final AllowableValue ALLOWABLE_VALUE_MQTT_VERSION_310 =
+            new AllowableValue(String.valueOf(MqttConnectOptions.MQTT_VERSION_3_1),
+                                      "v3.1.0");
+
 }
